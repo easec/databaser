aaa8a9a45442487fa0e607b5aa35578458cbd67b
easec <mats.johannesson@easec.se>
2022-02-15 08:41:44 +0100

    Update event.sql
    Add some events

--- mysql/event.sql
+++ mysql/event.sql
@@ -1,19 +1,19 @@
-CREATE DATABASE event;
+CREATE DATABASE event_db;
 
-CREATE TABLE `event`.`event`(
+CREATE TABLE `event_db`.`event_tbl`(
   `eventId` INT NOT NULL AUTO_INCREMENT,
   `titel` VARCHAR(50) NOT NULL,
   `datum` DATE NOT NULL,
   `pris` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`eventId`));
 
-CREATE TABLE `event`.`person` (
+CREATE TABLE `event_db`.`person_tbl` (
   `personId` INT NOT NULL AUTO_INCREMENT,
   `fNamn` VARCHAR(50) NOT NULL,
   `eNamn` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`personId`));
 
-CREATE TABLE `event`.`deltagare` (
+CREATE TABLE `event_db`.`deltagare_tbl` (
   `deltagarId` INT NOT NULL AUTO_INCREMENT,
   `event` INT NOT NULL,
   `person` INT NOT NULL,
@@ -30,3 +30,11 @@ CREATE TABLE `event`.`deltagare` (
     REFERENCES `event`.`person` (`personId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+ 
+INSERT INTO `event_db`.`event_tbl` (titel, datum, pris)
+VALUES
+('Databashantering', 220310, 10000),
+('Java', 220311, 5000),
+('Klienhantering', 220509, 15000)
+
+
