a7eb703cb1d1d0c6046a3955ce648f44c2e9f02a
easec <mats.johannesson@easec.se>
2022-02-16 20:50:50 +0100

    Update kontakt.sql

--- mysql/kontakt.sql
+++ mysql/kontakt.sql
@@ -2,26 +2,26 @@ CREATE DATABASE IF NOT EXISTS kontakt_db DEFAULT CHARACTER SET 'utf8mb4';
 
 
 CREATE TABLE `kontakt_db`,`meddelande_tbl` (    
-	`meddelande_Id` int NOT NULL AUTO_INCREMENT,
-	`anv_Id` int DEFAULT 0 NOT NULL,
-	`anv_Namn` varchar(50) DEFAULT `` NOT NULL,
-	`anv_Mail` varchar(50) DEFAULT `` NOT NULL,
-	`anv_Meddelande` varchar(200),
-	`anv_MeddTS` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`meddelande_Id` INT NOT NULL AUTO_INCREMENT,
+	`anv_Id` INT DEFAULT 0 NOT NULL,
+	`anv_Namn` VARCHAR(50) DEFAULT `` NOT NULL,
+	`anv_Mail` VARCHAR(50) DEFAULT `` NOT NULL,
+	`anv_Meddelande` VARCHAR(200),
+	`anv_MeddTS` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	PRIMARY KEY (`meddelande_Id`)
 	);
 
 CREATE TABLE `kontakt.db`,`anv_tbl` (
-	`anv_id` int NOT NULL AUTO_INCREMENT,
-        `fnamn` varchar(50) NOT NULL,
-        `enamn` varchar(50) NOT NULL,
-	`adress1` varchar(50) NOT NULL,
-        `adress2` varchar(50),
-        `adress3` varchar(50),
-        `pnummer` int NOT NULL,
-        `postort` varchar(40) NOT NULL,
-        `epost` varchar(40) NOT NULL,
-        `ftelefon` varchar(15),
-        `mtelefon` varchar(15),
+	`anv_id` INT NOT NULL AUTO_INCREMENT,
+        `fnamn` VARCHAR(50) NOT NULL,
+        `enamn` VARCHAR(50) NOT NULL,
+	`adress1` VARCHAR(50) NOT NULL,
+        `adress2` VARCHAR(50),
+        `adress3` VARCHAR(50),
+        `pnummer` INT NOT NULL,
+        `postort` VARCHAR(40) NOT NULL,
+        `epost` VARCHAR(40) NOT NULL,
+        `ftelefon` VARCHAR(15),
+        `mtelefon` VARCHAR(15),
         PRIMARY KEY (`anv_id`)
         );
