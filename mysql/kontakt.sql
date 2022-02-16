2de768017ce87344f82a4a5de9f0324ebe98cb89
easec <mats.johannesson@easec.se>
2022-02-16 20:43:57 +0100

    Create kontakt.sql

---
+++ mysql/kontakt.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS kontakt_db DEFAULT CHARACTER SET 'utf8mb4';
+
+CREATE TABLE `kontakt_db`,`meddelande_tbl` (    
+	`meddelande_Id` int NOT NULL auto_increment,
+	`anv_Id` int DEFAULT 0 NOT NULL,
+	`anv_Namn` varchar(50) DEFAULT `` NOT NULL,
+	`anv_Mail` varchar(50) DEFAULT `` NOT NULL,
+	`anv_Meddelande` varchar(200),
+	`anv_MeddTS` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	PRIMARY KEY (`meddelande_Id`)
+	);
+
+CREATE TABLE `kontakt.db`,`anv_tbl` (
+	`anv_id` int NOT NULL auto_increment,
+        `fnamn` varchar(50) NOT NULL,
+        `enamn` varchar(50) NOT NULL,
+	`adress1` varchar(50) NOT NULL,
+        `adress2` varchar(50),
+        `adress3` varchar(50),
+        `pnummer` int NOT NULL,
+        `postort` varchar(40) NOT NULL,
+        `epost` varchar(40) NOT NULL,
+        `ftelefon` varchar(15),
+        `mtelefon` varchar(15),
+        PRIMARY KEY (`anv_id`)
+        );
